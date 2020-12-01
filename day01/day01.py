64ffbee98fd4f841ebadee880dc8ebcbd3595a2e
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-01 18:26:15 +0000

    Day 1 solution

---
+++ day01/day01.py
@@ -0,0 +1,15 @@
+from sys import path
+
+with open(path[0] +"/input.txt") as f:
+    expenses = [int(x) for x in f]
+
+for i in expenses:
+    if 2020 - i in expenses:
+        print(i * (2020 - i))
+        break
+
+for i in expenses:
+    for j in expenses:
+        if 2020 - i - j in expenses:
+            print(i *  j * (2020 - i - j))
+            exit()
