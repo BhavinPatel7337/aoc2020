9d4ef1dfef6da7654cc9a6cd8a4c3394b48d5aee
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-02 21:30:23 +0000

    Refactored day 2

--- day02/day02.py
+++ day02/day02.py
@@ -4,5 +4,5 @@ import re
 with open(path[0] +"/input.txt") as f:
     passwords = [re.match(r'(\d+)\-(\d+) (\w): (\w+)', x).groups() for x in f]
 
-print(len([p for p in passwords if int(p[0]) <= p[3].count(p[2]) <= int(p[1])]))
-print(len([p for p in passwords if (p[3][int(p[0]) - 1] == p[2]) ^ (p[3][int(p[1]) - 1] == p[2])]))
+print(sum(int(p[0]) <= p[3].count(p[2]) <= int(p[1]) for p in passwords))
+print(sum((p[3][int(p[0]) - 1] == p[2]) ^ (p[3][int(p[1]) - 1] == p[2]) for p in passwords))
