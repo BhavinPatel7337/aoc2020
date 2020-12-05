43ca6b8f24eea74129001a2592548825d0e99e0d
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-05 19:57:47 +0000

    Day 5 solution

---
+++ day05/day05.py
@@ -0,0 +1,7 @@
+from sys import path
+
+with open(path[0] + '/input.txt') as f:
+    passes = [int(x.translate(str.maketrans('FBLR', '0101')), 2) for x in f]
+
+print(max(passes))
+print(set(range(min(passes), max(passes) + 1)) - set(passes))
