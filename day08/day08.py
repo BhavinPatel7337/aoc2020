abe10fb61058663189c426e4699316f9baf7b43a
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-08 19:31:41 +0000

    Modified day 8

--- day08/day08.py
+++ day08/day08.py
@@ -4,8 +4,7 @@ with open(path[0] + '/input.txt') as f:
     program = [(x[:3], int(x[4:])) for x in f]
 
 def run(program):
-    acc = 0
-    pc = 0
+    acc, pc = 0, 0
     executed = set()
     while pc not in executed and pc < len(program):
         executed.add(pc)
@@ -19,10 +18,12 @@ def run(program):
 
 print(run(program)[0])
 
+swap = {'nop': 'jmp', 'jmp': 'nop'}
+
 for n, i in enumerate(program):
-    if i[0] == 'jmp' or i[0] == 'nop':
-        new_program = program.copy()
-        new_program[n] = (i[0].translate(str.maketrans('jmno', 'nojm')), i[1])
-        result = run(new_program)
+    if i[0] in swap:
+        program[n] = (swap[i[0]], i[1])
+        result = run(program)
         if (result[1]) == len(program):
             print(result[0])
+        program[n] = i
