f981c2adab6a90ea313aa9e7d87347985aadf29d
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-08 02:18:19 +0000

    Modified day 7

--- day07/day07.py
+++ day07/day07.py
@@ -2,7 +2,7 @@ from sys import path
 import re
 
 with open(path[0] + '/input.txt') as f:
-    rules = dict((y[0], re.findall(r'(\d+) (\w+ \w+) bag', y[1])) for x in f if (y := x.split(' bags contain ')))
+    rules = dict((y[0][1], y[1:]) for x in f if (y := re.findall(r'(^|\d+ )(\w+ \w+) bag', x)))
 
 def parent_bags(rules, colour):
     return [k for k, v in rules.items() if any(i[1] == colour for i in v)]
@@ -10,11 +10,10 @@ def parent_bags(rules, colour):
 checklist = ['shiny gold']
 baglist = set()
 while checklist:
-    if (c := checklist.pop()) not in baglist:
-        checklist += parent_bags(rules, c)
-        baglist.add(c)
-
-print(len(baglist - {'shiny gold'}))
+    if (colour := checklist.pop()) not in baglist:
+        checklist += parent_bags(rules, colour)
+        baglist.add(colour)
+print(len(baglist) - 1)
 
 def count_child_bags(rules, colour):
     return 1 + sum(int(i[0]) * count_child_bags(rules, i[1]) for i in rules[colour])
