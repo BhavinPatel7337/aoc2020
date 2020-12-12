d793785cac42ad3efa70440afb5b44bbba3c375a
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-12 21:04:10 +0000

    Modified day 11 again

--- day11/day11.py
+++ day11/day11.py
@@ -15,30 +15,26 @@ def find_visible(x, y, dx, dy):
         i += 1
         j += 1
     if (x + dx * i, y + dy * j) in grid:
-        return [(x + dx * i, y + dy * j)]
-    else:
-        return []
+        return x + dx * i, y + dy * j
 
-visible = {}
-for x, y in grid:
-    if grid[x, y] != '.':
-        lst = []
-        for dx, dy in directions:
-            lst += find_visible(x, y, dx, dy)
-        visible[x, y] = lst
+visible = dict(((x, y), [find_visible(x, y, dx, dy) for dx, dy in directions]) for x, y in adjacent)
 
-def iterate(grid):
+def iterate(grid, neighbours, tolerance):
     new_grid = {}
     for x, y in grid:
-        if grid[x, y] == 'L' and sum(grid.get(i) == '#' for i in visible[x, y]) == 0:
+        if grid[x, y] == 'L' and sum(grid.get(i) == '#' for i in neighbours[x, y]) == 0:
             new_grid[x, y] = '#'
-        elif grid[x, y] == '#' and sum(grid.get(i) == '#' for i in visible[x, y]) > 4:
+        elif grid[x, y] == '#' and sum(grid.get(i) == '#' for i in neighbours[x, y]) > tolerance:
             new_grid[x, y] = 'L'
         else:
             new_grid[x, y] = grid[x, y]
     return new_grid
 
-while grid != iterate(grid):
-    grid = iterate(grid)
+def solve(part, grid):
+    params = {1: (adjacent, 3), 2: (visible, 4)}
+    while grid != (g := iterate(grid, *(params[part]))):
+            grid = g
+    return sum(i == '#' for i in grid.values())
 
-print(sum(i == '#' for i in grid.values()))
+print(solve(1, grid.copy()))
+print(solve(2, grid.copy()))
