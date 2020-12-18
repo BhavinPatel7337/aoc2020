50da9fc1a624f6170a8dbe9ef53d72abea7e60e2
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-18 00:49:13 +0000

    Modified day 17

--- day17/day17.py
+++ day17/day17.py
@@ -1,40 +1,36 @@
 from sys import path
 
-grid, adjacent = {}, {}
+active = set()
+with open(path[0] + '/input.txt') as f:
+    for y, row in enumerate(f):
+        for x, state in enumerate(row.strip()):
+            if state == '#': 
+                active.add((x, y, 0, 0))
+initial_size = (x + 1, y + 1)
+
 directions = [(x, y, z, w) for x in range(-1, 2)
                 for y in range(-1, 2)
                 for z in range(-1, 2)
                 for w in range(-1, 2)
                 if any([x != 0, y != 0, z != 0, w != 0])]
 
-with open(path[0] + '/input.txt') as f:
-    for y, row in enumerate(f):
-        for x, state in enumerate(row.strip()):
-            grid[x, y, 0, 0] = state
-            adjacent[x, y, 0, 0] = [(x + dx, y + dy, dz, dw) for dx, dy, dz, dw in directions]
-max_x, max_y, max_z = x, y, 0
+def iterate(active, wrange, cycle):
+    global initial_size
+    new_active = set()
+    for w in wrange:
+        for z in range(-cycle, cycle + 1):
+            for y in range(-cycle, initial_size[1] + cycle):
+                for x in range(-cycle, initial_size[0] + cycle):
+                    neighbours = [(x + dx, y + dy, z + dz, w + dw) for dx, dy, dz, dw in directions]
+                    active_neighbours = sum(i in active for i in neighbours)
+                    if active_neighbours == 3 or ((x, y, z, w) in active and active_neighbours == 2):
+                        new_active.add((x, y, z, w))
+    return new_active
 
-def iterate(grid):
-    global max_x, max_y, max_z
-    max_x += 1
-    max_y += 1
-    max_z += 1
-    new_grid = {}
-    for w in range(-max_z, max_z + 1):
-        for z in range(-max_z, max_z + 1):
-            for y in range(-max_z, max_y + max_z + 1):
-                for x in range(-max_z, max_x + max_z + 1):
-                    if (x, y, z, w) not in grid:
-                        adjacent[x, y, z, w] = [(x + dx, y + dy, z + dz, w + dw) for dx, dy, dz, dw in directions]
-                    cube = grid.get((x, y, z, w), '.')
-                    if cube == '#' and sum(grid.get(i) == '#' for i in adjacent[x, y, z, w]) not in [2, 3]:
-                        new_grid[x, y, z, w] = '.'
-                    elif cube == '.' and sum(grid.get(i) == '#' for i in adjacent[x, y, z, w]) == 3:
-                        new_grid[x, y, z, w] = '#'
-                    else:
-                        new_grid[x, y, z, w] = cube
-    return new_grid
-
-for i in range(6):
-    grid = iterate(grid)
-print(sum(i == '#' for i in grid.values()))
+part1 = active.copy()
+part2 = active.copy()
+for i in range(1, 7):
+    part1 = iterate(part1, range(1), i)
+    part2 = iterate(part2, range(-i, i + 1), i)
+print(len(part1))
+print(len(part2))
