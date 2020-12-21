8505b668b66c67aa99e4ebc724b583bf23ce65a8
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-21 23:50:17 +0000

    Modified day 20

--- day20/day20.py
+++ day20/day20.py
@@ -5,7 +5,7 @@ with open(path[0] + '/input.txt') as f:
     tiles = {int(x[0]): x[1].split() for x in re.findall(r'Tile (\d+):\n(.+?)(\n\n|$)', f.read(), re.S)}
 
 def rotate(tile):
-    return [''.join(r) for r in zip(*tile[::-1])]
+    return [''.join(x) for x in zip(*tile[::-1])]
 
 def flip(tile):
     return [x[::-1] for x in tile]
@@ -13,10 +13,10 @@ def flip(tile):
 def trim(tile):
     return [x[1:-1] for x in tile[1:-1]]
 
-def find_left(tid, orientation, flipped):
-    left = (orientation - 1 + 2 * flipped) % 4
+def find_right(tid, orientation, flipped):
+    right = (orientation - 1 + 2 * flipped) % 4
     for bid, binfo in borders[tid].items():
-        if binfo[0] == left:
+        if binfo[0] == right:
             return bid, (orientation + binfo[1] - binfo[0] + 2 + 2 * binfo[2]) % 4, binfo[2] ^ flipped
     return None
 
@@ -30,15 +30,14 @@ def find_below(tid, orientation, flipped):
 def find_sea_monster(image):
     sea_monster = [(0, 0), (-18, 1), (-13, 1), (-12, 1), (-7, 1), (-6, 1), (-1, 1), (0, 1), (1, 1), (-17, 2), (-14, 2), (-11, 2), (-8, 2), (-5, 2), (-2, 2)]
     occupied = set()
-    for attempt in range(4):
+    for attempt in range(8):
         for j, row in enumerate(image):
             for i, pixel in enumerate(row):
                 if i > 18 and i < len(row) - 1 and j < len(image) - 3 and pixel == '#':
                     if all(image[j + s[1]][i + s[0]] == '#' for s in sea_monster):
                         occupied |= set((i + s[0], j + s[1]) for s in sea_monster)
-        if attempt % 2 == 0:
-            image = rotate(image)
-        else:
+        image = rotate(image)
+        if attempt == 3:
             image = flip(image)
     return occupied
 
@@ -58,7 +57,7 @@ for x, r1 in rotations.items():
 
 product = 1
 for c in corners:
-    if find_left(c, 0, 0) and find_below(c, 0, 0):
+    if find_right(c, 0, 0) and find_below(c, 0, 0):
         start = (c, 0, False)
     product *= c
 print(product)
@@ -71,8 +70,8 @@ for i in range(len(tiles)):
     if flipped:
         tile = flip(tile)
     row = [''.join(x) for x in zip(row, trim(tile))]
-    if (left := find_left(current, orientation, flipped)):
-        current, orientation, flipped = left
+    if (right := find_right(current, orientation, flipped)):
+        current, orientation, flipped = right
     else:
         if (start := find_below(*start)):
             current, orientation, flipped = start
