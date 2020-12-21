9ed9a3c70f20a6e446af76aa7bde5561d5c1c84b
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2020-12-21 20:44:36 +0000

    Modified day 21

--- day21/day21.py
+++ day21/day21.py
@@ -7,9 +7,7 @@ with open(path[0] + '/input.txt') as f:
 allergens = {}
 for f in foods:
     for a in f[1]:
-        if a not in allergens:
-            allergens[a] = f[0].copy()
-        allergens[a] &= f[0]
+        allergens[a] = allergens.get(a, f[0].copy()) & f[0]
 
 for a, i in allergens.items():
     if len(i) == 1:
@@ -17,5 +15,5 @@ for a, i in allergens.items():
             if a2 != a:
                 allergens[a2] -= i
 
-print(len([x for l in [list(y[0]) for y in foods] for x in l if x not in set.union(*(allergens.values()))]))
+print(len([x for l in [y[0] for y in foods] for x in l if x not in set.union(*(allergens.values()))]))
 print(','.join([allergens[a].pop() for a in sorted(allergens)]))
