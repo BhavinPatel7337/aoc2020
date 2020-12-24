a90478bb13600b7d27ddab9397bec03349abf88e
Bhavin Patel <bpatel@foregenix.com>
2020-12-24 21:01:59 +0000

    Modified day 24

--- day24/day24.py
+++ day24/day24.py
@@ -4,6 +4,9 @@ import re
 with open(path[0] + '/input.txt') as f:
     tiles = [re.findall(r'[sn]?[ew]', x) for x in f]
 
+#     nw(0,1)     ne(1,1)
+# w(-1,0)     o(0,0)      e(1,0)
+#     sw(-1,-1)   se(0,-1)
 directions = {
     'e': (1, 0),
     'ne': (1, 1),
@@ -12,19 +15,11 @@ directions = {
     'sw': (-1, -1),
     'se': (0, -1)
 }
-#     nw(0,1)     ne(1,1)
-# w(-1,0)     o(0,0)      e(1,0)
-#     sw(-1,-1)   se(0,-1)
 
 black = {}
 for ti in tiles:
-    t = (0,0)
-    for i in ti:
-        d = directions[i]
-        t = (t[0] + d[0], t[1] + d[1])
+    t = tuple(sum(j) for j in zip(*[directions[i] for i in ti]))
     black[t] = black.get(t, False) ^ True
-
-print(sum(black.values()))
 
 def minmax():
     x, y = zip(*black)
@@ -32,8 +27,6 @@ def minmax():
 
 def adjacent(x, y):
     return sum(black.get((x + d[0], y + d[1]), False) for d in directions.values())
-
-min_x, max_x, min_y, max_y = minmax()
 
 def flip():
     new_black = {}
@@ -48,6 +41,8 @@ def flip():
             else:
                 new_black[x, y] = current
     return new_black
+
+print(sum(black.values()))
 
 for _ in range(100):
     black  = flip()
